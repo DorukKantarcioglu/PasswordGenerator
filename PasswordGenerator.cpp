a97a49c7b80e801c8f299a8d618de73a5397ec9d
Doruk Kantarcıoğlu <63489557+DorukKantarcioglu@users.noreply.github.com>
2021-05-30 15:47:43 +0300

    Update PasswordGenerator.cpp

--- PasswordGenerator.cpp
+++ PasswordGenerator.cpp
@@ -27,16 +27,16 @@ int main(int argc, char** argv)
     cout << endl;
     /// Empty the contents of the clipboard
     OpenClipboard(NULL);
-	EmptyClipboard();
-	HGLOBAL handle = GlobalAlloc(GMEM_MOVEABLE, passwordLength + 1);    /// Allocate sufficient bytes for the password
-	if (handle) {
+    EmptyClipboard();
+    HGLOBAL handle = GlobalAlloc(GMEM_MOVEABLE, passwordLength + 1);    /// Allocate sufficient bytes for the password
+    if (handle) {
        	memcpy(GlobalLock(handle), password, passwordLength + 1);
         GlobalUnlock(handle);
         SetClipboardData(CF_TEXT, handle);  /// Copy the password to the clipboard
         GlobalFree(handle); /// Free the allocated memory
-	}
-	CloseClipboard();
-	cout << "Password copied to clipboard." << endl;
+    }
+    CloseClipboard();
+    cout << "Password copied to clipboard." << endl;
     delete[] password;
 
     return 0;
